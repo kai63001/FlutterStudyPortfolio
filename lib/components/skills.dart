2f1232ed916f46f931d6ab739926c5d569270046
62021506 <62021506@up.ac.th>
2021-08-01 21:09:52 +0700

    🔥 update skills

--- lib/components/skills.dart
+++ lib/components/skills.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Skills extends StatelessWidget {
+class Skills extends StatefulWidget {
   const Skills({
     Key? key,
     required this.size,
@@ -11,21 +10,81 @@ class Skills extends StatelessWidget {
   final Size size;
 
   @override
+  _SkillsState createState() => _SkillsState();
+}
+
+class _SkillsState extends State<Skills> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 15.0),
-          child: Container(
-            width: size.width * 0.9,
-            child: Text(
-              'SKILLS',
-              style: GoogleFonts.fredokaOne(
-                textStyle: TextStyle(
-                    color: Colors.black, letterSpacing: 2.5, fontSize: 18),
+    List<dynamic> data = [
+      {"n": 0,"image":"assets/images/typescript.png", "name": "TYPESCRIPT"},
+      {"n": 1,"image":"assets/images/javascript.png", "name": "JAVASCRIPT"},
+      {"n": 2,"image":"assets/images/golang.png", "name": "GOLANG"},
+      {"n": 3,"image":"assets/images/c.png", "name": "C#"},
+      {"n": 4,"image":"assets/images/php.png", "name": "PHP"},
+      {"n": 5,"image":"assets/images/python.png", "name": "PYTHON"},
+      {"n": 6,"image":"assets/images/java.png", "name": "JAVA"},
+    ];
+    return Column(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 15.0),
+            child: Container(
+              width: widget.size.width * 0.9,
+              child: Text(
+                'SKILLS',
+                style: GoogleFonts.fredokaOne(
+                  textStyle: TextStyle(
+                      color: Colors.black, letterSpacing: 2.5, fontSize: 18),
+                ),
               ),
             ),
           ),
         ),
-      );
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Container(
+              height: 100,
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  for (var i in data) boxSkills(i),
+                ],
+              )),
+        )
+      ],
+    );
+  }
+
+  Padding boxSkills(data) {
+    var leftP = data["n"] == 0 ? 25.0 : 0.0;
+    return Padding(
+      padding: EdgeInsets.only(left: leftP, right: 8),
+      child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(data["image"]),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  data["name"],
+                  style: GoogleFonts.mitr(
+                    textStyle: TextStyle(color: Colors.white, fontSize: 13),
+                  ),
+                ),
+              ),
+            ],
+          ))),
+    );
   }
 }
